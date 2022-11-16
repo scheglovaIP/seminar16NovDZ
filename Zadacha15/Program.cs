985c88588f1bd53747909c47bdaebafd0d930cca
Irina <ip_scheglova@mail.ru>
2022-11-16 22:23:58 +0500

    add Home Work Number 15

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+Console.Write("Введите цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 1 && number <= 5)
+
+    Console.WriteLine($"{number} - будний день");
+
+else if(number==6 || number==7) Console.WriteLine($"{number} - выходной день");
+
+else
+
+    Console.WriteLine("Такого дня не существует");
