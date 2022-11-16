8c085de1b693b9684ab2ca3638a79ab8c7870fa5
Irina <ip_scheglova@mail.ru>
2022-11-16 22:02:56 +0500

    add Home Work Number 13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number>99)
+{
+    while (number>999)
+    {
+        number = number/10;
+    }
+    Console.WriteLine(number%10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
