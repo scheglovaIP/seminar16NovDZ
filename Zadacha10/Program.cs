febeba1aaa94baf3c3b2372062ddec00fd178db3
Irina <ip_scheglova@mail.ru>
2022-11-16 17:14:53 +0500

    add Home Work Number 10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number<99 || number>1000)
+{
+    Console.WriteLine("Введенное число не трехзначное");
+}
+
+else
+{
+    Console.WriteLine(number/10%10);
+}
